0ffc0752959222b2ef1c65368092a341ac44b62a
Eduardo Rodríguez Arribas <edura@gmail.com>
2022-02-23 17:39:11 +0100

    Se reproduce correctamente.

--- app/src/main/java/com/example/isonidos/MainActivity.java
+++ app/src/main/java/com/example/isonidos/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.isonidos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +20,12 @@ public class MainActivity extends AppCompatActivity {
         int idSonido = R.raw.siu;
         m = MediaPlayer.create(this,idSonido);
         m.start();
+    }
+    public void reproduceVideo(View vista){
+        VideoView videoView=findViewById(R.id.videoView);
+        int idVideo=this.getResources().getIdentifier(vista.getTag().toString(),"raw",this.getPackageName());
+        Uri uri= Uri.parse("android.resource://"+ getPackageName()+ "/" + idVideo);
+        videoView.setVideoURI(uri);
+        videoView.start();
     }
 }
